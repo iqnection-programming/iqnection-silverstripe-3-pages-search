bcb38eb636e608df3f4ace91a69f058f899abda5
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:09:08 +1300

    Converted to PSR-2

--- code/Page_Search_Extension.php
+++ code/Page_Search_Extension.php
@@ -1,15 +1,13 @@
 <?php
 
-	class Page_Search_Extension extends Extension
-	{
-		
-	}
-	
-	class Page_Controller_Search_Extension extends Extension
-	{
-		function SiteSearchPage()
-		{
-			return DataObject::get_one('SearchResultsPage');
-		}		
-	}
-	
+    class Page_Search_Extension extends Extension
+    {
+    }
+    
+    class Page_Controller_Search_Extension extends Extension
+    {
+        public function SiteSearchPage()
+        {
+            return DataObject::get_one('SearchResultsPage');
+        }
+    }
